e82a48e61b252e3a4eeeb7716e6b5d09da0b7ddd
Daniel Pereira Lima <limadanielpereira@gmail.com>
2025-01-21 17:49:34 -0300

    Adiciona Sobre Mim

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,8 +1,15 @@
+import PostModelo from "componentes/PostModelo"
+import fotoCapa from "assets/sobre_mim_capa.png"
+import styles from "./SobreMim.module.css"
 export default function SobreMim() {
     return (
-        <main>
-            <h1>Sobre mim</h1>
-        </main>
+        <PostModelo fotoCapa={fotoCapa}>
+            <h3 className={styles.subtitulo}>
+                Olá, eu sou o Daniel!
+            </h3>
+
+            
+        </PostModelo>
         
     )
 }
